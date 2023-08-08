21c137deb9eddea2cec1797740c7fa9a2b4be0f2
Chris Nolan <72170535+cnolan-nt@users.noreply.github.com>
2023-08-08 16:55:50 -0600

    Update RemoveDefenderExclusions.ps1

--- Remove McAfee/RemoveDefenderExclusions.ps1
+++ Remove McAfee/RemoveDefenderExclusions.ps1
@@ -1,5 +1,7 @@
 #!ps
 
-
 $Paths=(Get-MpPreference).ExclusionPath
 foreach ($Path in $Paths) { Remove-MpPreference -ExclusionPath $Path }
+
+$Processes=(Get-MpPreference).ExclusionProcess 
+foreach ($Path in $Paths) { Remove-MpPreference -ExclusionProcess $Processes }
